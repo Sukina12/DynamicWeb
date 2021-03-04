312914513abc6b31ab1fbbd8f1baf2574863c787
Sukina12 <sukina_89@yahoo.com>
2021-03-04 14:49:54 +0400

    change

---
+++ app.js
@@ -0,0 +1,26 @@
+alert ("Welcome To Our Cafe");
+
+var order = prompt("Enter Your Order For Brice:");
+
+if (order == "cofee")
+{alert ("$4.25") }
+
+else 
+     if (order == "Iced Coffee")
+     {alert ("$3.25")}
+
+     else {alert ("not available")}
+
+
+
+     
+     var txt;
+     var r = confirm("Press a button!");
+     if (r == true) {
+         document.write ("Welcome and Reurn Back");
+
+     } else {
+       document.write("Thank You for Your Support!! Delivery Available") 
+    
+
+     }
